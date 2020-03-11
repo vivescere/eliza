a7673dae19abd41f6f8448af491b2828952c4345
Nicolas Bourras <vivescere@gmail.com>
2020-03-11 13:13:28 +0100

    Made Eliza::interact handle quit words

--- src/eliza.rs
+++ src/eliza.rs
@@ -19,10 +19,24 @@ impl Eliza {
     }
 
     pub fn interact(&self, input: &str) -> Response {
+        let input = input.trim().to_lowercase();
+        let words: Vec<&str> = input.split(" ").collect();
+
+        if words.len() == 1 && self.rules.quit.iter().any(|w| w == words[0]) {
+            let message = self
+                .rules
+                .final_
+                .choose(&mut rand::thread_rng())
+                .expect("final rules should have at least one item");
+
+            return Response::farewell(message.to_string());
+        }
+
         Response::normal("INTERACT".to_string())
     }
 }
 
+#[derive(Debug, PartialEq, Eq)]
 pub struct Response {
     pub is_farewell: bool,
     pub message: String,
@@ -41,5 +55,31 @@ impl Response {
             message,
             is_farewell: true,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_interact_quit() {
+        let rules = Rules {
+            initial: vec![],
+            final_: vec!["goodbye".to_string()],
+            quit: vec!["quit".to_string()],
+            pre: vec![],
+            post: vec![],
+            synonyms: vec![],
+            keywords: vec![],
+        };
+
+        let eliza = Eliza::new(rules);
+
+        assert!(!eliza.interact("hello quit").is_farewell);
+        assert_eq!(
+            Response::farewell("goodbye".to_string()),
+            eliza.interact("quit")
+        );
     }
 }
